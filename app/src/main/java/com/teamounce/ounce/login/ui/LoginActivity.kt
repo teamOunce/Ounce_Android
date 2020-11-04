f68cf36908759fabda88b6c67bd9460f14037aba
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-11-04 21:52:27 +0900

    #3 style: Complete login layout

---
+++ app/src/main/java/com/teamounce/ounce/login/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.teamounce.ounce.login.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamounce.ounce.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
