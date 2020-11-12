1ad15378880441b48f14522394de1b6653b76aea
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-11-12 17:59:44 +0900

    #13 feat: Google login add

--- app/src/main/java/com/teamounce/ounce/login/ui/LoginActivity.kt
+++ app/src/main/java/com/teamounce/ounce/login/ui/LoginActivity.kt
@@ -4,12 +4,24 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
@@ -21,6 +33,13 @@ import com.teamounce.ounce.util.TransparentStatusBarObject
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val loginViewModel: LoginViewModel by viewModels()
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,14 +51,65 @@ class LoginActivity : AppCompatActivity() {
 
         binding.dotsLoginOnboarding.setViewPager2(binding.vpLoginOnboarding)
 
-        binding.btnLoginKakao.setOnClickListener {
-            kakaoLoginCall(this)
-        }
+        binding.btnLoginKakao.setOnClickListener { kakaoLoginCall(this) }
 
         binding.btnTempNext.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+        binding.btnLoginGoogle.setOnClickListener { signIn() }
+        auth = Firebase.auth
+
+        binding.btnTempDisconnectGoogle.setOnClickListener { disconnectGoogle() }
+    }
+
+    private fun disconnectGoogle() {
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this) {
+            Toast.makeText(this, "Disconnect From Google", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account?.idToken!!)
+            } catch (e: ApiException) {
+                Log.w("Login", "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    val user = auth.currentUser
+                    //updateUI(user)
+                    loginViewModel.setCurrentUser(user)
+                    val intent = Intent(this, SignUpActivity::class.java)
+                    Toast.makeText(this, "${user?.email}", Toast.LENGTH_SHORT).show()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Auth Fail", Toast.LENGTH_SHORT).show()
+                }
+
+            }
     }
 
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
@@ -56,11 +126,17 @@ class LoginActivity : AppCompatActivity() {
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) Log.e("Kakao", "로그인 실패", error)
             else if (token != null) {
-                Log.i("Kakao", "로그인 성공 ${token.accessToken}, ${token.refreshToken}, ${token.scopes}")
+                Log.i(
+                    "Kakao",
+                    "로그인 성공 ${token.accessToken}, ${token.refreshToken}, ${token.scopes}"
+                )
                 requestAdditionalUserInfo()
             }
         }
-        if (LoginClient.instance.isKakaoTalkLoginAvailable(context)) LoginClient.instance.loginWithKakaoTalk(context, callback = callback)
+        if (LoginClient.instance.isKakaoTalkLoginAvailable(context)) LoginClient.instance.loginWithKakaoTalk(
+            context,
+            callback = callback
+        )
         else LoginClient.instance.loginWithKakaoAccount(context, callback = callback)
     }
 
@@ -69,13 +145,18 @@ class LoginActivity : AppCompatActivity() {
             if (error != null) {
                 Log.e("Kakao", "사용자 정보 요청 실패", error)
             } else if (user != null) {
-                if(user.kakaoAccount?.emailNeedsAgreement == false) Log.d("Kakao", "사용자계정에 이메일 없음")
-                else if(user.kakaoAccount?.emailNeedsAgreement == true) Log.d("Kakao", "사용자에게")
-                Log.i("Kakao", "${user.id}, ${user.kakaoAccount?.email}, ${user.kakaoAccount?.profile?.nickname}")
+                if (user.kakaoAccount?.emailNeedsAgreement == false) Log.d("Kakao", "사용자계정에 이메일 없음")
+                else if (user.kakaoAccount?.emailNeedsAgreement == true) Log.d("Kakao", "사용자에게")
+                Log.i(
+                    "Kakao",
+                    "${user.id}, ${user.kakaoAccount?.email}, ${user.kakaoAccount?.profile?.nickname}"
+                )
             }
         }
     }
+
     companion object {
         private const val NUM_PAGES = 3
+        private const val RC_SIGN_IN = 9001
     }
 }
