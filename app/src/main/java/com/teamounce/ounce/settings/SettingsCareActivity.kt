90bea78a5720ed76cc190cd6478d5f22631c7c67
JooYae <jooyae3970@naver.com>
2020-11-26 02:42:52 +0900

    SettingsActivity

---
+++ app/src/main/java/com/teamounce/ounce/settings/SettingsCareActivity.kt
@@ -0,0 +1,12 @@
+package com.teamounce.ounce.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamounce.ounce.R
+
+class SettingsCareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_care)
+    }
+}
