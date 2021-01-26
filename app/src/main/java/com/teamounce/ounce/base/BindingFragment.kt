2016c984029aee9207f1ba41f3834c1a371511f6
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-26 20:08:51 +0900

    #95 feat : DataBinding Base ViewController 만들기

---
+++ app/src/main/java/com/teamounce/ounce/base/BindingFragment.kt
@@ -0,0 +1,39 @@
+package com.teamounce.ounce.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+abstract class BindingFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    protected lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    protected inner class LifeCycleEventLogger(private val className: String) : LifecycleObserver {
+        fun registerLogger(lifecycle: Lifecycle) {
+            lifecycle.addObserver(this)
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+        fun log() {
+            Log.d("${className}LifeCycleEvent", "${lifecycle.currentState}")
+        }
+    }
+}
