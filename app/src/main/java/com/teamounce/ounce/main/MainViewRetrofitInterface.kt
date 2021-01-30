a2148c0d78044c1b6d6817575ea187ab6a4e3eb2
JooYae <jooyae3970@naver.com>
2021-01-30 13:40:37 +0900

    token

--- app/src/main/java/com/teamounce/ounce/main/MainViewRetrofitInterface.kt
+++ app/src/main/java/com/teamounce/ounce/main/MainViewRetrofitInterface.kt
@@ -5,12 +5,12 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface MainViewRetrofitInterface {
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMTkyNjk2MH0.VU4UPyKVZ-20aMKU41vdpSUPZc-nOsxA7NzJD0vlRHI"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjA2NjE2Mn0.ZkeFUdDckkKg-NQajUyVdmukvdknM3MY-SQIwgU1DHs"])
     @GET("/main")
     fun mainViewRetrofit(@Query("catIndex") catIndex: Int): Call<MainViewResponseData>
 
 
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMTkyNjk2MH0.VU4UPyKVZ-20aMKU41vdpSUPZc-nOsxA7NzJD0vlRHI"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjA2NjE2Mn0.ZkeFUdDckkKg-NQajUyVdmukvdknM3MY-SQIwgU1DHs"])
     @GET("/main/getList")
     fun bottomSheetProfileRetrofit(@Query("catIndex") catIndex: Int): Call<BottomSheetResponseData>
 }
