a26c621607147cbdc7257e85900546a32b3b949a
JooYae <jooyae3970@naver.com>
2021-02-03 17:56:36 +0900

    chore : ounceforyou instagram uri

--- app/src/main/java/com/teamounce/ounce/main/MainViewRetrofitInterface.kt
+++ app/src/main/java/com/teamounce/ounce/main/MainViewRetrofitInterface.kt
@@ -5,16 +5,16 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface MainViewRetrofitInterface {
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjIzNzY1NH0.6jHdQeHw7bEsQrcMfxZPbEq7oR3M-q06U-WH3MMjhSQ"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjQyNjIwNn0.3bPZZWkcFv6iR0x14jsQgXivAL34EWsM5nwmz-Yp2oA"])
     @GET("/main")
     fun mainViewRetrofit(@Query("catIndex") catIndex: Int): Call<MainViewResponseData>
 
 
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjIzNzY1NH0.6jHdQeHw7bEsQrcMfxZPbEq7oR3M-q06U-WH3MMjhSQ"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjQyNjIwNn0.3bPZZWkcFv6iR0x14jsQgXivAL34EWsM5nwmz-Yp2oA"])
     @GET("/main/getList")
     fun bottomSheetProfileRetrofit(@Query("catIndex") catIndex: Int): Call<BottomSheetResponseData>
 
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjIzNzY1NH0.6jHdQeHw7bEsQrcMfxZPbEq7oR3M-q06U-WH3MMjhSQ"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjQyNjIwNn0.3bPZZWkcFv6iR0x14jsQgXivAL34EWsM5nwmz-Yp2oA"])
     @DELETE("/main/profile")
     fun mainDeleteRetrofit(@Query("catIndex") catIndex: Int): Call<MainDeleteResponseData>
 
--- app/src/main/java/com/teamounce/ounce/settings/SettingButtonAdapter.kt
+++ app/src/main/java/com/teamounce/ounce/settings/SettingButtonAdapter.kt
@@ -1,15 +1,21 @@
 package com.teamounce.ounce.settings
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.teamounce.ounce.R
 
 class SettingButtonAdapter (private val context:Context): RecyclerView.Adapter<SettingButtonViewHolder>(){
     var datas = mutableListOf<SettingButtonData>()
+    val uriInsta : Uri = Uri.parse("https://instagram.com/ounceforyou?igshid=1bypchn4t7xme")
+    val forApp = Intent(Intent.ACTION_VIEW , uriInsta)
+
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): SettingButtonViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_setting_btn, p0,false)
@@ -24,6 +30,14 @@ class SettingButtonAdapter (private val context:Context): RecyclerView.Adapter<S
                 0 -> {
                     val intent = Intent(context, SettingsCareActivity::class.java)
                     context.startActivity(intent)
+                }
+                1 -> {
+                    try {
+                        context.startActivity(forApp)
+                    }catch (e:ActivityNotFoundException){
+                        startActivity ( context ,forApp , null )
+                    }
+
                 }
 
                 3->{
