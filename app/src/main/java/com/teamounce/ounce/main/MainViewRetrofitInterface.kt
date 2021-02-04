76f16c5b10c424c6d5ab1289e5abe415eb31b584
JooYae <jooyae3970@naver.com>
2021-02-04 20:08:07 +0900

    resolve conflict

--- app/src/main/java/com/teamounce/ounce/main/MainViewRetrofitInterface.kt
+++ app/src/main/java/com/teamounce/ounce/main/MainViewRetrofitInterface.kt
@@ -5,16 +5,16 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface MainViewRetrofitInterface {
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjQyNjIwNn0.3bPZZWkcFv6iR0x14jsQgXivAL34EWsM5nwmz-Yp2oA"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjUyMTU2M30.MdVPQ7vOXiBBT9NMbyfrfHwQPNu3adb-42HdNSy7Oxc"])
     @GET("/main")
     fun mainViewRetrofit(@Query("catIndex") catIndex: Int): Call<MainViewResponseData>
 
 
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjQyNjIwNn0.3bPZZWkcFv6iR0x14jsQgXivAL34EWsM5nwmz-Yp2oA"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjUyMTU2M30.MdVPQ7vOXiBBT9NMbyfrfHwQPNu3adb-42HdNSy7Oxc"])
     @GET("/main/getList")
     fun bottomSheetProfileRetrofit(@Query("catIndex") catIndex: Int): Call<BottomSheetResponseData>
 
-    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjQyNjIwNn0.3bPZZWkcFv6iR0x14jsQgXivAL34EWsM5nwmz-Yp2oA"])
+    @Headers(value = ["Content-Type: application/json", "token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPdW5jZSIsInVzZXJJZHgiOjksImV4cCI6MTYxMjUyMTU2M30.MdVPQ7vOXiBBT9NMbyfrfHwQPNu3adb-42HdNSy7Oxc"])
     @DELETE("/main/profile")
     fun mainDeleteRetrofit(@Query("catIndex") catIndex: Int): Call<MainDeleteResponseData>
 
