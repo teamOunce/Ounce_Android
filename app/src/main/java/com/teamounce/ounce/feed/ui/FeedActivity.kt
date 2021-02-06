10731418a1b9412b4db1052d63ae3f737af2b472
Kangmingu <kangmin1012>
2021-02-06 15:49:45 +0900

    #128 [FEAT] : Feed Activity 생성

---
+++ app/src/main/java/com/teamounce/ounce/feed/ui/FeedActivity.kt
@@ -0,0 +1,17 @@
+package com.teamounce.ounce.feed.ui
+
+import android.os.Bundle
+import com.teamounce.ounce.R
+import com.teamounce.ounce.base.BindingActivity
+import com.teamounce.ounce.databinding.ActivityFeedBinding
+import com.teamounce.ounce.util.TransparentStatusBarObject
+
+class FeedActivity :BindingActivity<ActivityFeedBinding>(R.layout.activity_feed) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        TransparentStatusBarObject.setStatusBar(this)
+        binding.apply {
+            lifecycleOwner = this@FeedActivity
+        }
+    }
+}
