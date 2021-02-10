c9670893be9cd21a6eea57a1678bdd57f9a805d5
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-02-10 21:23:37 +0900

    #146 [STYLE] : RecordActivity.kt UI 작업

--- app/src/main/java/com/teamounce/ounce/review/ui/RecordActivity.kt
+++ app/src/main/java/com/teamounce/ounce/review/ui/RecordActivity.kt
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.teamounce.ounce.R
+import com.teamounce.ounce.base.BindingActivity
 import com.teamounce.ounce.databinding.ActivityRecordBinding
 
-class RecordActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityRecordBinding
+class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_record) {
     private val foodImages = mutableListOf<ImageSliderFragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_record)
         foodImages.add(ImageSliderFragment("https://www.meijer.com/content/dam/meijer/product/0082/92/7450/22/0082927450225_1_A1C1_1200.png"))
         binding.vpRecordSlider.adapter = CatFoodImageSliderPagerAdapter(this)
     }
