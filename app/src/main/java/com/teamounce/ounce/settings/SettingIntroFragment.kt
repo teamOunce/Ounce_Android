100665775fec3fd72cfdae6ac0bf34078d005ad0
JooYae <jooyae3970@naver.com>
2021-02-11 17:28:30 +0900

    fragment item, backgroundcolor

--- app/src/main/java/com/teamounce/ounce/settings/SettingIntroFragment.kt
+++ app/src/main/java/com/teamounce/ounce/settings/SettingIntroFragment.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.teamounce.ounce.R
+import com.teamounce.ounce.databinding.ItemOunceintroBinding
 
-class SettingIntroFragment: Fragment() {
+class SettingIntroFragment(private val src : Int): Fragment() {
+    private lateinit var binding : ItemOunceintroBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.item_ounceintro,container,false)
+    ): View ?{
+        binding = DataBindingUtil.inflate(inflater, R.layout.item_ounceintro,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initImageView()
+    }
+    fun initImageView(){
+        binding.ounceIntroImage.setImageResource(src)
+    }
+
 }
