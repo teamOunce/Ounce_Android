f88d08887e272d31e6f71f6e37d2657543fdf6a2
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-02-13 18:25:31 +0900

    #172 [FIX] : 에러나는 부분 HOTFIX

--- app/src/main/java/com/teamounce/ounce/settings/SettingsIntroActivity.kt
+++ app/src/main/java/com/teamounce/ounce/settings/SettingsIntroActivity.kt
@@ -1,5 +1,6 @@
 package com.teamounce.ounce.settings
 
+
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +12,9 @@ import androidx.viewpager2.widget.ViewPager2
 import com.teamounce.ounce.R
 import com.teamounce.ounce.databinding.ActivitySettingsIntroBinding
 import kotlinx.android.synthetic.main.activity_settings_intro.*
-import kotlinx.android.synthetic.main.item_ounceintro.*
 
-class SettingsIntroActivity : AppCompatActivity(){
-    private lateinit var viewPager : ViewPager2
+class SettingsIntroActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
     private lateinit var binding: ActivitySettingsIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,35 +24,34 @@ class SettingsIntroActivity : AppCompatActivity(){
         initViewPager()
     }
 
-    fun initViewPager(){
+    private fun initViewPager() {
         viewPager = binding.vpIntroOunce
         val pagerAdapter = SettingIntroAdapter(this)
         viewPager.adapter = pagerAdapter
     }
 
-    private inner class SettingIntroAdapter ( fa : FragmentActivity) : FragmentStateAdapter(fa) {
-        //private var layoutInflater : LayoutInflater?=null
+    private inner class SettingIntroAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
         override fun getItemCount(): Int = NUM_PAGES
 
-        override fun createFragment(position: Int) {
-            return when(position){
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
                 0 -> {
-                    SettingIntroFragment(R.drawable.who_made_ounce_img_1)
                     ounceintro_background.setBackgroundColor(Color.parseColor("#9CFF60"))
+                    SettingIntroFragment(R.drawable.who_made_ounce_img_1)
                 }
                 1 -> {
-                    SettingIntroFragment(R.drawable.who_made_ounce_img_2)
                     ounceintro_background.setBackgroundColor(Color.parseColor("#C98AEC"))
+                    SettingIntroFragment(R.drawable.who_made_ounce_img_2)
                 }
                 else -> {
-                    SettingIntroFragment(R.drawable.who_made_ounce_img_3)
                     ounceintro_background.setBackgroundColor(Color.parseColor("#D47272"))
+                    SettingIntroFragment(R.drawable.who_made_ounce_img_3)
                 }
             }
         }
-
     }
-    companion object{
+
+    companion object {
         private const val NUM_PAGES = 3
     }
 
