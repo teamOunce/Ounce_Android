cb8a99b2ef81b96795c36cfe12ea6d9f4c370329
kangmingu <kangmin1012@naver.com>
2022-01-20 22:16:36 +0900

    Version Code up

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 31
     const val minSdk = 26
     const val targetSdk = 30
-    const val versionCode = 12
-    const val versionName = "1.2.0"
+    const val versionCode = 13
+    const val versionName = "1.2.1"
 }
