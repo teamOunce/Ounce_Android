2b361fdd37a288a97cdaae2d78a4fdb832b75b6e
kangmin1012 <kangmin1012@naver.com>
2022-04-05 21:46:38 +0900

    SerializedName 적용 안된 부분 추가 적용

--- app/src/main/java/com/teamounce/ounce/feed/model/ResponseFeedReviewData.kt
+++ app/src/main/java/com/teamounce/ounce/feed/model/ResponseFeedReviewData.kt
@@ -1,21 +1,37 @@
 package com.teamounce.ounce.feed.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ResponseFeedReviewData(
+    @SerializedName("data")
     val data: List<Data>,
+    @SerializedName("responseMessage")
     val responseMessage: String
 ) {
     data class Data(
+        @SerializedName("createdDate")
         val createdDate: String,
+        @SerializedName("manufacturer")
         val manufacturer: String,
+        @SerializedName("memo")
         val memo: String,
+        @SerializedName("myImg")
         val myImg: String,
+        @SerializedName("preference")
         val preference: Int,
+        @SerializedName("productImg")
         val productImg: String,
+        @SerializedName("productName")
         val productName: String,
+        @SerializedName("reviewIndex")
         val reviewIndex: Int,
+        @SerializedName("tag1")
         val tag1: String,
+        @SerializedName("tag2")
         val tag2: String,
+        @SerializedName("tag3")
         val tag3: String,
+        @SerializedName("updatedDate")
         val updatedDate: String
     )
 }
--- app/src/main/java/com/teamounce/ounce/feed/model/ResponseFilterData.kt
+++ app/src/main/java/com/teamounce/ounce/feed/model/ResponseFilterData.kt
@@ -1,12 +1,19 @@
 package com.teamounce.ounce.feed.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ResponseFilterData(
+    @SerializedName("data")
     val data: Data,
+    @SerializedName("responseMessage")
     val responseMessage: String
 ) {
     data class Data(
+        @SerializedName("manu")
         val manu: List<String>,
+        @SerializedName("tag")
         val tag: List<String>,
+        @SerializedName("type")
         val type: List<String>
     )
 }
