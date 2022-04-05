45e84d3bae9eab55955fe997385eef1d876530de
kangmin1012 <kangmin1012@naver.com>
2022-04-05 21:35:12 +0900

    SerializedName 어노테이션 추가

--- app/src/main/java/com/teamounce/ounce/main/MainViewCatInfoData.kt
+++ app/src/main/java/com/teamounce/ounce/main/MainViewCatInfoData.kt
@@ -1,7 +1,12 @@
 package com.teamounce.ounce.main
 
+import com.google.gson.annotations.SerializedName
+
 data class MainViewCatInfoData(
+    @SerializedName("catName")
     val catName : String,
+    @SerializedName("fromMeet")
     val fromMeet : Int,
+    @SerializedName("reviewCount")
     val reviewCount : Int
 )
