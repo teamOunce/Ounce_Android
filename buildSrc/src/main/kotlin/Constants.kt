fdd4ae8f8731fe48d7e001ddb99ac3febabdd169
kangmin1012 <kangmin1012@naver.com>
2022-05-03 11:49:27 +0900

    Base Url 변경

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 31
     const val minSdk = 26
     const val targetSdk = 30
-    const val versionCode = 18
+    const val versionCode = 19
     const val versionName = "1.4.3"
 }
